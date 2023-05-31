9f607199d5e62a7a0ced509a8a1c2e660147c589
Edis Syuleyman <edis.suleyman1010@gmail.com>
2023-06-01 02:24:50 +0300

    Applied style to repairs

--- repairs/create.php
+++ repairs/create.php
@@ -4,43 +4,64 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	<link rel="stylesheet" href="../style.css">
 </head>
 <body>
-	<form method="POST" action="<?php $_PHP_SELF ?>">
-		<label>Дата</label><input type="date" name="date"><br>
-		<label>Клиент</label>
-		<select name="client">
-			<?php include '../config.php';
-			$result = $dbConn->query("SELECT * FROM `Clients`");
-			while ($row = mysqli_fetch_assoc($result)) {
-				$id = $row['id_client'];
-				$name = $row['first_name']." ".$row['last_name'];
-				echo "<option value='$id'>$name</option>";
-			}
-			?>
-		</select><br>
-		<label>Служител</label>
-		<select name="employee">
-			<?php include '../config.php';
-			$result = $dbConn->query("SELECT * FROM `Employees`");
-			while ($row = mysqli_fetch_assoc($result)) {
-				$id = $row['id_employee'];
-				$name = $row['first_name']." ".$row['last_name'];
-				echo "<option value='$id'>$name</option>";
-			}
-			?>
-		</select><br>
-		<select multiple="multiple" name="services[]">
-			<?php include '../config.php';
-			$result = $dbConn->query("SELECT * FROM `Services`");
-			while ($row = mysqli_fetch_assoc($result)) {
-				$id = $row['id_service'];
-				$name = $row['name'];
-				echo "<option value='$id'>$name</option>";
-			}
-			?>
-		</select><br>
-		<input type="submit" name="create" value="Създай">
+	<section class="top-nav">
+        <div>
+            <h1 onclick="redirectToSubPageRelative('index')">Сами строй</h1>
+        </div>
+        <input id="menu-toggle" type="checkbox" />
+        <label class='menu-button-container' for="menu-toggle">
+            <div class='menu-button'></div>
+        </label>
+        <ul class="menu">
+            <li><button type="button" onclick="redirectToPage('../service_groups','index')">Категории услуги</button></li>
+            <li><button type="button" onclick="redirectToPage('../services','index')">Услуги</button></li>
+            <li><button type="button" onclick="redirectToPage('../employee_positions','index')">Позиции на служители</button></li>
+            <li><button type="button" onclick="redirectToPage('../employees','index')">Служители</button></li>
+            <li><button type="button" onclick="redirectToPage('../clients','index')">Клиенти</button></li>
+            <li><button type="button" onclick="redirectToPage('../repairs','index')">Ремонти</button></li>
+        </ul>
+    </section>
+	<form method="POST" action="<?php $_PHP_SELF ?>" id="msform">
+		<fieldset>
+			<input type="date" name="date"><br>
+			<select name="client">
+				<option value="" disabled selected>Изберете клиент</option>
+				<?php include '../config.php';
+				$result = $dbConn->query("SELECT * FROM `Clients`");
+				while ($row = mysqli_fetch_assoc($result)) {
+					$id = $row['id_client'];
+					$name = $row['first_name']." ".$row['last_name'];
+					echo "<option value='$id'>$name</option>";
+				}
+				?>
+			</select><br>
+			<select name="employee">
+				<option value="" disabled selected>Изберете служител</option>
+				<?php include '../config.php';
+				$result = $dbConn->query("SELECT * FROM `Employees`");
+				while ($row = mysqli_fetch_assoc($result)) {
+					$id = $row['id_employee'];
+					$name = $row['first_name']." ".$row['last_name'];
+					echo "<option value='$id'>$name</option>";
+				}
+				?>
+			</select><br>
+			<label>Изберете услуги</label>
+			<select multiple="multiple" name="services[]">
+				<?php include '../config.php';
+				$result = $dbConn->query("SELECT * FROM `Services`");
+				while ($row = mysqli_fetch_assoc($result)) {
+					$id = $row['id_service'];
+					$name = $row['name'];
+					echo "<option value='$id'>$name</option>";
+				}
+				?>
+			</select><br>
+			<input type="submit" name="create" value="Създай">
+		</fieldset>
 	</form>
 	<?php
 		include '../config.php';
@@ -77,5 +98,6 @@
 			header("Location: index.php");
 		}
 	 ?>
+<script src="../functions.js"></script>
 </body>
 </html>
